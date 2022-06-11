af543db5bc60a6bd2842d8df15ed675282710219
Julio Acuña <lititosalta@gmail.com>
2022-06-11 00:11:41 -0300

    Añadiendo Archivo Java

---
+++ GitPrimerVez.java
@@ -0,0 +1,12 @@
+import javax.xml.transform.SourceLocator;
+
+public class GitPrimerVez {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+		
+		System.out.println("Este es el primer paso que doy en GIT");
+		
+	}
+
+}
