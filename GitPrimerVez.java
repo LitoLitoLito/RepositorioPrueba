e8a54f6b24b5846a0727a7cdeda7e5bc36491001
LitoLitoLito <107225758+LitoLitoLito@users.noreply.github.com>
2022-06-11 01:14:17 -0300

    Update GitPrimerVez.java
    Sigo en Eclipse

--- GitPrimerVez.java
+++ GitPrimerVez.java
@@ -3,8 +3,11 @@ import javax.xml.transform.SourceLocator;
 public class GitPrimerVez {
 
 	public static void main(String[] args) {
-		// TODO Esbozo de mÈtodo generado autom·ticamente
+		// TODO Esbozo de m√©todo generado autom√°ticamente
 		
+		System.out.println("Este es el primer paso que doy en GIT");
+		System.out.println("Este es el primer paso que doy en GIT");
+		System.out.println("Este es el primer paso que doy en GIT");
 		System.out.println("Este es el primer paso que doy en GIT");
 		
 	}
